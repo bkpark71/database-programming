50fe8bd0a2d1844d0961c1f0b166c3a9b2453b1d
bkpark <bokyoungp@naver.com>
2023-09-11 14:36:25 +0900

    database프로그래밍 v2.0

--- src/madang/MainApp.java
+++ src/madang/MainApp.java
@@ -1,16 +1,21 @@
 package madang;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainApp {
   static Connection conn = DBConn.makeConnection();
+  static Scanner in = new Scanner(System.in);
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+
     int menu =0;
     if(conn != null) {
       while(true){
-        System.out.print("메뉴를 선택해주세요(1:도서리스트, 2:고객리스트, 3:주문리스트 (종료하려면 -1) : ");
+        System.out.println("메뉴를 선택해주세요 (종료하려면 -1) ");
+        System.out.println("(1:도서리스트, 2:고객리스트, 3:주문리스트 ");
+        System.out.print("11:도서추가등록, 12:고객추가등록, 13:주문등록) : ");
         menu = in.nextInt();
         if(menu < 0){
           System.out.println("프로그램을 종료합니다.");
@@ -27,6 +32,15 @@ public class MainApp {
             case 3:
               orderList();
               break;
+            case 11:
+              addBook();
+              break;
+            case 12:
+              addCustomer();
+              break;
+            case 13:
+              addOrder();
+              break;
           }
         } catch (SQLException e) {
           throw new RuntimeException(e);
@@ -35,6 +49,106 @@ public class MainApp {
     }
   }
 
+  private static void addBook() throws SQLException {
+    String sql = "SELECT max(bookid) FROM book;";
+    PreparedStatement pstmt = conn.prepareStatement(sql);
+    ResultSet rs = pstmt.executeQuery();
+    int bookid = 0, res = 0;
+    String[] bookinfos;
+    if(rs != null && rs.next()) {
+      bookid = rs.getInt(1);
+    }
+    System.out.print("등록할 책 정보를 책이름, 출판사, 가격 순으로 입력하세요.");
+    bookinfos = in.next().split(",");
+    sql = "insert into book values (?,?,?,?);";
+    pstmt = conn.prepareStatement(sql);
+    pstmt.setInt(1,bookid + 1);
+    pstmt.setString(2,bookinfos[0]);
+    pstmt.setString(3,bookinfos[1]);
+    pstmt.setInt(4,Integer.parseInt(bookinfos[2]));
+    res = pstmt.executeUpdate();
+    if(res == 1) System.out.println("등록완료!");
+  }
+
+  private static void addCustomer() throws SQLException {
+    String sql = "SELECT max(custid) FROM customer;";
+    PreparedStatement pstmt = conn.prepareStatement(sql);
+    ResultSet rs = pstmt.executeQuery();
+    int custid = 0, res = 0;
+    String[] custinfos;
+    if(rs != null && rs.next()) {
+      custid = rs.getInt(1);
+    }
+    System.out.print("등록할 고객 정보를 고객이름, 주소, 전화번호 순으로 입력하세요.");
+    custinfos = in.next().split(",");
+    sql = "insert into customer values (?,?,?,?);";
+    pstmt = conn.prepareStatement(sql);
+    pstmt.setInt(1,custid + 1);
+    pstmt.setString(2,custinfos[0]);
+    pstmt.setString(3,custinfos[1]);
+    pstmt.setString(4,custinfos[2]);
+    res = pstmt.executeUpdate();
+    if(res == 1) System.out.println("등록완료!");
+  }
+
+  private static void addOrder() throws SQLException {
+    Integer res = 0 , custId = 0, bookId = 0, bookPrice = 0, salePrice = 0;
+    String[] orderinfos;
+    String sql="";
+    String name ="", bookInfo = "";
+
+    System.out.print("주문을 등록할 고객id,도서id를 입력하세요.");
+    orderinfos = in.next().split(",");
+    custId = Integer.parseInt(orderinfos[0]);
+    name = validCustomer(custId);
+    bookId = Integer.parseInt(orderinfos[1]);
+    bookInfo = validBook(bookId);
+    System.out.println(bookInfo);
+
+    if(name != null && bookInfo != null) {
+      String[] bookInfos = bookInfo.split(",");
+      String now = LocalDate.now().toString();
+      System.out.printf("고객명 : %s , 책이름 : %s, 가격 : %s 가 맞다면 판매가격을 입력하세요. ",
+          name , bookInfos[0], bookInfos[1]);
+      salePrice = in.nextInt();
+      sql = "insert into orders (custid, bookid, saleprice, orderdate, bname) values " +
+          "(?,?,?,?,?);";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setInt(1,custId);
+      pstmt.setInt(2,bookId);
+      pstmt.setInt(3,salePrice);
+      pstmt.setString(4, now);
+      pstmt.setString(5,bookInfos[0]);
+      res = pstmt.executeUpdate();
+      if(res == 1) System.out.println("등록완료!");
+    }
+  }
+  static String validCustomer(int custId) throws SQLException {
+    String sql = "select name from customer where custid = ?;";
+    PreparedStatement pstmt = conn.prepareStatement(sql);
+    pstmt.setInt(1,custId);
+    ResultSet rs = pstmt.executeQuery();
+
+    if(rs != null && rs.next()) {
+      return rs.getString(1);
+    }
+    return null;
+  }
+
+  private static String validBook(int bookId) throws SQLException {
+    String sql = "select bookname, price from book where bookid = ?;";
+    String bookInfo = "";
+    PreparedStatement pstmt = conn.prepareStatement(sql);
+    pstmt.setInt(1,bookId);
+    ResultSet rs = pstmt.executeQuery();
+
+    if(rs != null && rs.next()) {
+      bookInfo = rs.getString(1);
+      bookInfo += ",";
+      bookInfo += rs.getInt(2);
+    }
+    return bookInfo;
+  }
   static void bookList() throws SQLException {
     String sql = "SELECT * FROM book;";
     PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -48,9 +162,10 @@ public class MainApp {
         System.out.printf(" %7d ",rs.getInt("price"));
         System.out.printf("  %-20s ", rs.getString("bookname"));
         System.out.printf(" %-20s \n",rs.getString("publisher"));
-
       }
       System.out.println("---------------------------------------------------");
+    } else {
+      System.out.println("출력할 책 내역이 없습니다.");
     }
   }
 
@@ -70,6 +185,8 @@ public class MainApp {
 
       }
       System.out.println("-------------------------------------------");
+    } else {
+      System.out.println("출력할 고객 내역이 없습니다.");
     }
   }
 
@@ -91,6 +208,8 @@ public class MainApp {
         System.out.printf(" %-20s \n", rs.getString("bookname"));
       }
       System.out.println("---------------------------------------------------------------");
+    } else {
+      System.out.println("출력할 주문 내역이 없습니다.");
     }
   }
 }
